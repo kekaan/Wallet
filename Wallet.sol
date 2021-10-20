b4b519aecf814cc9a0f6beda202cf580a6c0778e
kekaan <iskandier_shakirov@mail.ru>
2021-10-21 01:56:58 +0300

    init

---
+++ Wallet.sol
@@ -0,0 +1,43 @@
+pragma ton-solidity >= 0.35.0;
+pragma AbiHeader expire;
+
+contract Wallet {
+
+    constructor() public {
+        require(tvm.pubkey() != 0, 101);
+        require(msg.pubkey() == tvm.pubkey(), 102);
+        tvm.accept();
+    }
+
+    modifier checkOwnerAndAccept {
+        require(msg.pubkey() == tvm.pubkey(), 100);
+		tvm.accept();
+		_;
+	}
+    
+
+    function sendTransactionPayingComissionWithBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, true, 1);
+    }
+
+    function sendTransactionNotPayingComissionWithBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, true, 0);
+    }
+    
+    function sendAllAndDestroyWalletWithBounce(address dest) public pure checkOwnerAndAccept {
+        dest.transfer(1, true, 160);
+    }
+
+        function sendTransactionPayingComissionNoBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, false, 1);
+    }
+
+    function sendTransactionNotPayingComissionNoBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+        dest.transfer(value, false, 0);
+    }
+    
+    function sendAllAndDestroyWalletNoBounce(address dest) public pure checkOwnerAndAccept {
+        dest.transfer(1, false, 160);
+    }
+
+}
