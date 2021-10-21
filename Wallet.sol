fa64380d02b1eec4a88836e98b12d1eb517daea3
kekaan <iskandier_shakirov@mail.ru>
2021-10-21 06:25:10 +0300

    Небольшие исправления

--- Wallet.sol
+++ Wallet.sol
@@ -16,28 +16,15 @@ contract Wallet {
 	}
     
 
-    function sendTransactionPayingComissionWithBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+    function sendTransactionPayingComission(address dest, uint128 value) public pure checkOwnerAndAccept {
         dest.transfer(value, true, 1);
     }
 
-    function sendTransactionNotPayingComissionWithBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
+    function sendTransactionNotPayingComission(address dest, uint128 value) public pure checkOwnerAndAccept {
         dest.transfer(value, true, 0);
     }
     
-    function sendAllAndDestroyWalletWithBounce(address dest) public pure checkOwnerAndAccept {
+    function sendAllAndDestroyWallet(address dest) public pure checkOwnerAndAccept {
         dest.transfer(1, true, 160);
     }
-
-    function sendTransactionPayingComissionNoBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
-        dest.transfer(value, false, 1);
-    }
-
-    function sendTransactionNotPayingComissionNoBounce(address dest, uint128 value) public pure checkOwnerAndAccept {
-        dest.transfer(value, false, 0);
-    }
-    
-    function sendAllAndDestroyWalletNoBounce(address dest) public pure checkOwnerAndAccept {
-        dest.transfer(1, false, 160);
-    }
-
 }
